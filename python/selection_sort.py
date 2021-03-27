3386998d9087de291201e3827c0a71cee7c17083
irostub <irostub@gmail.com>
2021-03-27 18:45:34 +0900

    Add selection sort algorithm

---
+++ python/selection_sort.py
@@ -0,0 +1,22 @@
+"""
+bubble sort algorithm
+
+CASE1
+input : 1 9 6 2 4 3
+output : [1, 2, 3, 4, 6, 9]
+"""
+
+
+def selection_sort(attrs=[]):
+    for i in range(len(attrs)):
+        for j in range(i + 1, len(attrs)):
+            if attrs[i] > attrs[j]:
+                temp = attrs[i]
+                attrs[i] = attrs[j]
+                attrs[j] = temp
+    return attrs
+
+
+if __name__ == '__main__':
+    nums = list(map(int, input().split()))
+    print(selection_sort(nums))
